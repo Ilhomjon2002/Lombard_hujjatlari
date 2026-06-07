b115fa6afe1b886f7e61218d7a2cfb9fb04a1c92
Ilhomjon <muqimovilhomjon61@example.com>
2026-06-07 16:17:37 +0500

    Allow Django to serve media files in production (DEBUG=False) for QR codes and uploads

--- config/urls.py
+++ config/urls.py
@@ -26,6 +26,13 @@ urlpatterns = [
     path('documents/', include('documents.urls')),
 ]
 
+from django.urls import re_path
+from django.views.static import serve
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
